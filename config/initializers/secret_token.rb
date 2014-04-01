746833f2b03c9de7043e98fb986970d0494af9d6
Victor Mutai <muvic08@gmail.com>
2014-04-01 14:17:32 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parking::Application.config.secret_key_base = 'ebc8eb999d4733428a71e128f7e3897e24b004faf391c3ac1d61e3189a81f67d4c7e0832649345e972c37af6c9af1cc24c0ea71fda80f6189cc75f46a1bba620'
